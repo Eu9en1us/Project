ee448d399c1b9d7ef8ada11f4d6177054ba39679
Eu9en1us <nobodyinnowere@gmail.com>
2023-04-21 19:35:56 +0300

    update all

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*var app = new VTTCue({
+    el:'#app',
+    data:{
+        products: [
+            {id:1, title:"Pear 1", short_text:'short_text', img:'../img/Pears/1.jpeg', desc:"Full desc"},
+            {id:2, title:"Pear 2", short_text:'short_text', img:'../img/Pears/2.webp', desc:"Full desc"},
+            {id:3, title:"Pear 3", short_text:'short_text', img:'../img/Pears/3.jpeg', desc:"Full desc"},
+            {id:4, title:"Pear 4", short_text:'short_text', img:'../img/Pears/4.jpeg', desc:"Full desc"},
+            {id:5, title:"Pear 5", short_text:'short_text', img:'../img/Pears/5.jpeg', desc:"Full desc"},
+        ]
+    },
+    mounted: function() {
+        console.log(window.localStorage.getItem('prod'));
+    },
+    methods: function(id) {
+        window.localStorage.setItem('prod', id);
+    }
+});*/
+
