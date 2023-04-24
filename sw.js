589ba13c9af84fdfcec18b1dbd4974ff94a4f9a9
Eu9en1us <nobodyinnowere@gmail.com>
2023-04-24 11:13:48 +0300

    update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
-/*self.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
-  });
-  self.addEventListener('activate', function(event) {
- console.log('[Service Worker] Activating Service Worker ...', event);
- return self.clients.claim();
- });
-   self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ....', event);
-    event.respondWith(fetch(event.request));
-  });
-*/
+    event.waitUntil(
+        caches.open("static")
+        .then(function(cache) {
+            console.log("precaching");
+            cache.add('/index.html');
+            cache.add('/index1.html');
+            cache.add('/index2.html');
+            cache.add('/index3.html');
+            cache.add('/css/main.css');
+            cache.add('/css/responsive.css');
+            cache.add('/img/Logo.svg');
+            cache.add('/');
+        })
+    )
+});
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if(response)
+                    return response;
+                else
+                    return fetch(event.request);
+            })
+    )
+});
