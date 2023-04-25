e3eb75b02652105afd3424651330363cda3fe55d
Eu9en1us <nobodyinnowere@gmail.com>
2023-04-25 10:33:32 +0300

    Update all

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
@@ -11,8 +10,7 @@ self.addEventListener('install', function(event) {
             cache.add('/index3.html');
             cache.add('/css/main.css');
             cache.add('/css/responsive.css');
-            cache.add('/manifest.json');
-            cache.add('/img/Logo.svg');
+            // cache.add('/img/Logo.svg'); його в требе немає, в тебе є /img/icons/logo.png
             cache.add('/');
         })
     )
@@ -31,38 +29,4 @@ self.addEventListener('fetch', function(event) {
                     return fetch(event.request);
             })
     )
-=======
-self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-    event.waitUntil(
-        caches.open("static")
-        .then(function(cache) {
-            console.log("precaching");
-            cache.add('/index.html');
-            cache.add('/index1.html');
-            cache.add('/index2.html');
-            cache.add('/index3.html');
-            cache.add('/css/main.css');
-            cache.add('/css/responsive.css');
-            cache.add('/manifest.json');
-            cache.add('/img/Logo.svg');
-            cache.add('/');
-        })
-    )
-});
-self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-    return self.clients.claim();
-});
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if(response)
-                    return response;
-                else
-                    return fetch(event.request);
-            })
-    )
->>>>>>> 9cc51741afe5035b7bae9d1b05da89b509806469
 });
